7d3981554ea684c1709a0eeeed0f4d900ef93343
Aleksey Monakhov <goshagriboedskii@gmail.com>
2022-09-23 01:43:32 +0300

    udp js

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,18 +1,21 @@
-const inputYear = document.querySelector("#Yearly");
-const inputMonth = document.querySelector("#Monthly");
+const inputYear = document.querySelector(".plans-tumbler-container");
 const prices = Array.from(document.querySelectorAll(".card-price"));
 
-inputYear.addEventListener("click", () => {
-  let counter = 0;
-  const pricesYear = ["$12", "$36", "$56"];
-  prices.forEach((price) => {
-    price.textContent = pricesYear[counter++];
-  });
-});
-inputMonth.addEventListener("click", () => {
+inputYear.addEventListener("click", (event) => {
   let counter = 0;
   const pricesMont = ["$19", "$54", "$89"];
-  prices.forEach((price) => {
-    price.textContent = pricesMont[counter++];
-  });
+  const pricesYear = ["$12", "$36", "$56"];
+  switch (event.target.value) {
+    case "month":
+      prices.forEach((price) => {
+        price.textContent = pricesMont[counter++];
+      });
+      break;
+
+    default:
+      prices.forEach((price) => {
+        price.textContent = pricesYear[counter++];
+      });
+      break;
+  }
 });
