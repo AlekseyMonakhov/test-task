cb1b92c649f98e70ce822be9f3d197ecef79eadf
Aleksey Monakhov <goshagriboedskii@gmail.com>
2022-09-25 11:39:23 +0300

    add animation

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,25 @@
 const inputYear = document.querySelector(".plans-tumbler-container");
 const prices = Array.from(document.querySelectorAll(".card-price"));
-
+const animation = {
+  duration: 250,
+  fill: "forwards",
+  easing: "ease",
+};
 inputYear.addEventListener("click", (event) => {
   let counter = 0;
   const pricesMont = ["$19", "$54", "$89"];
   const pricesYear = ["$12", "$36", "$56"];
-  switch (event.target.value) {
-    case "month":
-      prices.forEach((price) => {
-        price.textContent = pricesMont[counter++];
-      });
-      break;
 
-    default:
-      prices.forEach((price) => {
-        price.textContent = pricesYear[counter++];
-      });
-      break;
+  if (event.target.value === "month") {
+    prices.forEach((price) => {
+      price.textContent = pricesMont[counter++];
+      price.animate([{ opacity: 0 }, { opacity: 1 }], animation);
+    });
+  }
+  if (event.target.value === "year") {
+    prices.forEach((price) => {
+      price.textContent = pricesYear[counter++];
+      price.animate([{ opacity: 0 }, { opacity: 1 }], animation);
+    });
   }
 });
