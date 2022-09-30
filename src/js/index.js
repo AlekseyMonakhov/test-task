8bd4fbc3e97551eb5dfc2b80c04510d46542112f
Aleksey Monakhov <goshagriboedskii@gmail.com>
2022-09-30 18:15:28 +0300

    upd

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,18 @@
-const inputYear = document.querySelector(".plans-tumbler-container");
+const switchContainer = document.querySelector(".plans-tumbler-container");
 const prices = Array.from(document.querySelectorAll(".card-price"));
 const animation = {
   duration: 500,
   fill: "forwards",
   easing: "linear",
 };
-inputYear.addEventListener("click", (event) => {
+switchContainer.addEventListener("click", (event) => {
   let counter = 0;
   const pricesMont = ["$19", "$54", "$89"];
   const pricesYear = ["$12", "$36", "$56"];
-
-  if (event.target.value === "month") {
-    prices.forEach((price) => {
-      price.textContent = pricesMont[counter++];
-      price.animate([{ opacity: 0 }, { opacity: 1 }], animation);
-    });
-  }
-  if (event.target.value === "year") {
-    prices.forEach((price) => {
-      price.textContent = pricesYear[counter++];
-      price.animate([{ opacity: 0 }, { opacity: 1 }], animation);
-    });
-  }
+  prices.forEach((price) => {
+    price.textContent = (
+      event.target.value === "year" ? pricesYear : pricesMont
+    )[counter++];
+    price.animate([{ opacity: 0 }, { opacity: 1 }], animation);
+  });
 });
